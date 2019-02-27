27e68130e33227f82d00a9ba08a07cfd7b2fa562
zhaijunhua <3513125902@qq.com>
2019-02-27 16:08:25 +0800

    first-update

---
+++ pages/portal/portal.js
@@ -0,0 +1,74 @@
+// pages/portal/portal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img2.imgtn.bdimg.com/it/u=1167183091,4179730109&fm=200&gp=0.jpg',
+      'http://img3.imgtn.bdimg.com/it/u=496641143,3869583512&fm=200&gp=0.jpg',
+      'http://img1.imgtn.bdimg.com/it/u=725285042,1775347115&fm=26&gp=0.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载,用的比较多，后面的生命周期函数，可能会用在大型的项目中
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
